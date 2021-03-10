57290a6d3fec20c96ecefcf7663a20fbb24dcedb
William <william.marback@hotmail.com>
2021-03-10 09:57:58 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generiskaKlasser_ovning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
