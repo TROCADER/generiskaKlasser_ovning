7fe21b68d45f147aa4f4d329ed92a17c42e630c0
William <william.marback@hotmail.com>
2021-03-10 10:20:32 +0100

    Player able to buy item

--- Program.cs
+++ Program.cs
@@ -7,15 +7,24 @@ namespace generiskaKlasser_ovning
     {
         static void Main(string[] args)
         {
+            int playerCoins = 100;
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
 
-            dictionary.Add("Apple", 10);
-            dictionary.Add("Cucumber", 20);
+            dictionary.Add("apple", 10);
+            dictionary.Add("cucumber", 20);
 
             foreach (string key in dictionary.Keys)
             {
                 System.Console.WriteLine(key + ": " + dictionary[key] + " coins");
             }
+
+            System.Console.WriteLine("\n\nWhat to buy?\nType the desiered object");
+
+            string playerChoose = Console.ReadLine();
+
+            playerCoins -= dictionary[playerChoose];
+
+            System.Console.WriteLine("Thanks for your purchase!\nCoins left: " + playerCoins);
 
             Console.ReadLine();
         }
